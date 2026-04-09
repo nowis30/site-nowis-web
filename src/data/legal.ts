33e4b0605f3d11ebe793dd4ae2b368295cfa95fa
nowis30 <simonmorin30@gmail.com>
2026-04-09 14:48:13 -0400

    feat: ajouter le NEQ 2264731649 dans la configuration legale

--- src/data/legal.ts
+++ src/data/legal.ts
@@ -1,8 +1,8 @@
 export const legalConfig = {
   companyName: 'Création Nowis',
   legalName: 'Simon Morin',
-  businessIdLabel: 'NEQ / identifiant d’entreprise',
-  businessIdValue: 'Non applicable à ce jour',
+  businessIdLabel: 'NEQ (Numéro d'entreprise du Québec)',
+  businessIdValue: '2264731649',
   responsiblePrivacyTitle: 'Responsable de la protection des renseignements personnels',
   responsiblePrivacyName: 'Simon Morin',
   contactEmail: 'simonmorin@nowis.store',
